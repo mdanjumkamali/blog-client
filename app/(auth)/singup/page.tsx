631e894f5ceee1c5e70fea7656b9cbdd3f132a88
Md Anjum Kamali <mdanjumkamali@gmail.com>
2024-01-14 03:39:11 +0530

    Add Singup page

---
+++ app/(auth)/singup/page.tsx
@@ -0,0 +1,114 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import Login from "../login/page";
+
+const formSchema = z.object({
+  name: z.string().min(4, {
+    message: "Name is required",
+  }),
+  email: z.string().min(2, {
+    message: "Email is required",
+  }),
+  password: z.string().min(4, {
+    message: "Password is required!",
+  }),
+});
+
+const Singup = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      name: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+  return (
+    <div className="flex h-screen">
+      {/* form */}
+      <div className="w-[40%] flex items-center justify-center">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 w-[80%]"
+          >
+            <FormDescription className="text-2xl text-black">
+              Signup
+            </FormDescription>
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Anjum Kamali" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="anjum@email.com" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="anjum123" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+            <FormDescription>
+              Already have account?
+              <Link href={"/login"} className="text-black">
+                {" "}
+                Login
+              </Link>
+            </FormDescription>
+          </form>
+        </Form>
+      </div>
+
+      {/* img */}
+      <div className="w-[60%] bg-red-200"></div>
+    </div>
+  );
+};
+
+export default Singup;
