810afc97c70d4e60f01e94e324bac154f51d9245
Md Anjum Kamali <mdanjumkamali@gmail.com>
2024-01-18 04:22:53 +0530

    Add tags

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -9,6 +9,10 @@ interface SidebarItem {
   icons: string;
 }
 
+interface SidebarTag {
+  tag: string;
+}
+
 const data: SidebarItem[] = [
   {
     id: 1,
@@ -39,6 +43,21 @@ const data: SidebarItem[] = [
   // Add more items as needed
 ];
 
+const tag: SidebarTag[] = [
+  {
+    tag: "#frontend",
+  },
+  {
+    tag: "#backend",
+  },
+  {
+    tag: "#web3",
+  },
+  {
+    tag: "#ai",
+  },
+];
+
 const Sidebar = () => {
   const [active, setActive] = useState<number | null>(null);
   const onActive = (id: number) => {
@@ -46,20 +65,32 @@ const Sidebar = () => {
   };
   return (
     <div>
-      {data.map((item) => (
-        <div
-          key={item.id}
-          className={
-            active === item.id
-              ? "flex items-center gap-2 dark mb-2 rounded-md p-1 cursor-pointer bg-[#F2F1EB] text-black dark:bg-white dark:text-black"
-              : "flex items-center gap-2 dark mb-2 rounded-md p-1 cursor-pointer hover:bg-[#F2F1EB] hover:text-black dark:hover:bg-[#F2F1EB] dark:hover:text-black"
-          }
-          onClick={() => onActive(item.id)}
-        >
-          <Image src={item.icons} alt={item.title} width={30} height={30} />
-          <h4 className="text-lg font-semibold  ">{item.title}</h4>
-        </div>
-      ))}
+      {/* title */}
+      <div className="border-b-2 pb-6  border-black dark:border-white">
+        {data.map((item) => (
+          <div
+            key={item.id}
+            className={
+              active === item.id
+                ? "flex items-center gap-2 dark mb-2 rounded-md p-1 cursor-pointer bg-[#F2F1EB] text-black dark:bg-white dark:text-black"
+                : "flex items-center gap-2 dark mb-2 rounded-md p-1 cursor-pointer hover:bg-[#F2F1EB] hover:text-black dark:hover:bg-[#F2F1EB] dark:hover:text-black"
+            }
+            onClick={() => onActive(item.id)}
+          >
+            <Image src={item.icons} alt={item.title} width={30} height={30} />
+            <h4 className="text-lg font-semibold  ">{item.title}</h4>
+          </div>
+        ))}
+      </div>
+
+      {/* tags */}
+      <div>
+        {tag.map((item, index) => (
+          <li key={index} className="list-none mt-4 cursor-pointer">
+            {item.tag}
+          </li>
+        ))}
+      </div>
     </div>
   );
 };
