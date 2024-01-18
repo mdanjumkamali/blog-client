ebc8df35118d84f318edaf5e2f697811d7f583ea
Md Anjum Kamali <mdanjumkamali@gmail.com>
2024-01-18 06:00:14 +0530

    Refactor Dark Mode

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,20 +1,9 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import {
-  Cross1Icon,
-  HamburgerMenuIcon,
-  MoonIcon,
-  SunIcon,
-} from "@radix-ui/react-icons";
-import { useTheme } from "next-themes";
+import { Cross1Icon, HamburgerMenuIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
 import { useState } from "react";
+import DarkMode from "../darkmode/DarkMode";
 
 interface link {
   id: number;
@@ -51,7 +40,6 @@ const links: link[] = [
 ];
 
 const Navbar = () => {
-  const { setTheme } = useTheme();
   const [menu, setMenu] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [active, setActive] = useState<number | null>(null);
@@ -65,6 +53,8 @@ const Navbar = () => {
       <div className="text-2xl flex-1 md:flex-none md:text-4xl font-semibold ">
         Blog
       </div>
+
+      {/* navlink */}
       <div className="hidden md:flex items-center gap-8 flex-1 ml-9 text-lg">
         {links.map((link) => (
           <Link
@@ -83,27 +73,9 @@ const Navbar = () => {
       </div>
 
       {/* dark mode */}
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="outline" size="icon">
-            <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle theme</span>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuItem onClick={() => setTheme("light")}>
-            Light
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("dark")}>
-            Dark
-          </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("system")}>
-            System
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <DarkMode />
 
+      {/* login button */}
       <div className="hidden md:block ">
         <Button asChild>
           <Link href="/login">Login</Link>
